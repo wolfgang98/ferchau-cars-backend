0545d194999c58c19968a48a98cd8e79ba87a85a
Wolfgang Schuchter <wolfgang.98@hotmail.com>
2023-05-08 08:52:37 +0000

    initial setup

---
+++ src/api/rent/controllers/rent.ts
@@ -0,0 +1,7 @@
+/**
+ * rent controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::rent.rent');
---
+++ src/api/rent/services/rent.ts
@@ -0,0 +1,7 @@
+/**
+ * rent service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::rent.rent');
